eeadf338e9cf320fb5e311fcdc65ca30e0834792
Mario Davis <50406492+Scripsky@users.noreply.github.com>
2021-11-19 11:41:09 -0600

    Update Application_Uninstall.ps1

--- Remediation/Application_Uninstall.ps1
+++ Remediation/Application_Uninstall.ps1
@@ -1,17 +1,3 @@
-﻿$value = 0
+$application = Get-WmiObject -Class win32_product -ErrorAction SilentlyContinue | Where-Object -Property Name -EQ "Application Name" | Select-Object -ExpandProperty IdentifyingNumber
 
-do{
-    $application = Get-WmiObject -Class win32_product -ErrorAction SilentlyContinue | Where-Object -Property Name -EQ "Symantec Endpoint Protection" | Select-Object -ExpandProperty IdentifyingNumber
-
-    if ($application -ne $null -and $value -eq 0){
-        msiexec /x "$application" /q
-    }
-
-    if ($value -ge 15){
-        Write-Host "Maximimum Attempted Retries Reached"
-        exit 1
-    }
-
-    $value++
-    start-sleep 10
-}while($application -ne $null)
+start-process msiexec -ArgumentList /x, "$application", /qn, /norestart -wait
